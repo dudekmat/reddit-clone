04c06c75a4d3d88bcadad49d23ab970b3bede310
banzo276 <matdudek2706@gmail.com>
2022-11-06 19:26:46 +0100

    Added jwt authentication filter

--- src/main/java/com/banzo/reddit/config/SecurityConfig.java
+++ src/main/java/com/banzo/reddit/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.banzo.reddit.config;
 
+import com.banzo.reddit.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   private final UserDetailsService userDetailsService;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   @Bean
   public AuthenticationManager authenticationManager(HttpSecurity httpSecurity,
@@ -37,6 +40,9 @@ public class SecurityConfig {
         .permitAll()
         .anyRequest()
         .authenticated();
+
+    httpSecurity.addFilterBefore(jwtAuthenticationFilter,
+        UsernamePasswordAuthenticationFilter.class);
 
     return httpSecurity.build();
   }
