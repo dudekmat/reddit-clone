380730d5134a7ed4221056379af0a9184d60c87a
dudekmat <matdudek2706@gmail.com>
2023-01-01 00:23:44 +0100

    Added cors config

--- src/main/java/com/banzo/reddit/dto/RegistrationRequest.java
+++ src/main/java/com/banzo/reddit/dto/RegistrationRequest.java
@@ -3,11 +3,16 @@ package com.banzo.reddit.dto;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class RegistrationRequest {
 
   @NotBlank(message = "Username is required")
--- src/main/java/com/banzo/reddit/dto/SubredditPayload.java
+++ src/main/java/com/banzo/reddit/dto/SubredditPayload.java
@@ -1,11 +1,16 @@
 package com.banzo.reddit.dto;
 
 import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class SubredditPayload {
 
   @NotBlank(message = "Name is required")
