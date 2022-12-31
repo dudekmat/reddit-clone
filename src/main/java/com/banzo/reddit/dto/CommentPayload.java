78be9fb22b7c4e9b37dfa56796e704186292e3f2
dudekmat <matdudek2706@gmail.com>
2023-01-01 00:23:44 +0100

    Added cors config

--- src/main/java/com/banzo/reddit/dto/CommentPayload.java
+++ src/main/java/com/banzo/reddit/dto/CommentPayload.java
@@ -2,11 +2,16 @@ package com.banzo.reddit.dto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class CommentPayload {
 
   @NotNull(message = "Post id is required")
--- src/main/java/com/banzo/reddit/dto/LoginRequest.java
+++ src/main/java/com/banzo/reddit/dto/LoginRequest.java
@@ -1,11 +1,16 @@
 package com.banzo.reddit.dto;
 
 import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class LoginRequest {
 
   @NotBlank(message = "Username is required")
